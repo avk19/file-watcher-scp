8f92b267131195ac60a31908fa1e1b28aeeead36
avk19 <23744045+avk19@users.noreply.github.com>
2024-02-29 13:31:11 +0530

    Update EncryptionUtility.java

--- src/main/java/com/example/EncryptionUtility.java
+++ src/main/java/com/example/EncryptionUtility.java
@@ -1,9 +1,10 @@
-import org.apache.commons.crypto.Crypto;
 import org.apache.commons.crypto.cipher.CryptoCipher;
 import org.apache.commons.crypto.cipher.CryptoCipherFactory;
 import org.apache.commons.crypto.cipher.CryptoCipherFactory.CipherProvider;
 import org.apache.commons.crypto.utils.Utils;
 
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 import java.nio.charset.StandardCharsets;
 import java.security.SecureRandom;
 import java.util.Base64;
@@ -11,18 +12,17 @@ import java.util.Base64;
 public class EncryptionUtility {
 
     private static final String ALGORITHM = "AES";
-    private static final String TRANSFORMATION = "AES/ECB/PKCS5Padding";
-    private static final String SECRET_KEY_ALGORITHM = "PBKDF2WithHmacSHA256";
-    private static final int ITERATION_COUNT = 65536;
+    private static final String TRANSFORMATION = "AES/GCM/NoPadding";
     private static final int KEY_LENGTH = 256;
 
     public static String encrypt(String data, String password) throws Exception {
         byte[] salt = generateSalt();
-        byte[] key = Utils.pbkdf2(password, salt, ITERATION_COUNT, KEY_LENGTH);
+        byte[] key = Utils.pbkdf2(password, salt, 65536, KEY_LENGTH);
 
         CipherProvider provider = Utils.getCipherProvider();
         try (CryptoCipher encipher = CryptoCipherFactory.getCryptoCipher(ALGORITHM + TRANSFORMATION, provider)) {
-            encipher.init(Cipher.ENCRYPT_MODE, Utils.createKey(key), Utils.createNonce());
+            SecretKey secretKey = new SecretKeySpec(key, ALGORITHM);
+            encipher.init(Cipher.ENCRYPT_MODE, secretKey, Utils.createNonce());
 
             byte[] input = data.getBytes(StandardCharsets.UTF_8);
             byte[] output = new byte[encipher.getOutputSize(input.length)];
@@ -38,11 +38,12 @@ public class EncryptionUtility {
 
     public static String decrypt(String encryptedData, String password) throws Exception {
         byte[] salt = generateSalt();
-        byte[] key = Utils.pbkdf2(password, salt, ITERATION_COUNT, KEY_LENGTH);
+        byte[] key = Utils.pbkdf2(password, salt, 65536, KEY_LENGTH);
 
         CipherProvider provider = Utils.getCipherProvider();
         try (CryptoCipher decipher = CryptoCipherFactory.getCryptoCipher(ALGORITHM + TRANSFORMATION, provider)) {
-            decipher.init(Cipher.DECRYPT_MODE, Utils.createKey(key), Utils.createNonce());
+            SecretKey secretKey = new SecretKeySpec(key, ALGORITHM);
+            decipher.init(Cipher.DECRYPT_MODE, secretKey, Utils.createNonce());
 
             byte[] input = Base64.getDecoder().decode(encryptedData);
             byte[] output = new byte[decipher.getOutputSize(input.length)];
@@ -65,7 +66,7 @@ public class EncryptionUtility {
 
     public static void main(String[] args) {
         try {
-            String originalData = "Hello, encryption!";
+            String originalData = "Hello, strong encryption!";
             String password = "StrongPassword";
 
             String encryptedData = encrypt(originalData, password);
